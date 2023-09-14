0677911bcd4bd28859be47d3b0511423e6ab26e3
forsterit <olegkrivolap57@gmail.com>
2023-09-14 13:17:33 +0300

    Homework lesson 3

--- exercise_5.py
+++ exercise_5.py
@@ -7,13 +7,13 @@ stop_random = int(input('Введите конец: '))
 
 random_number = random.randrange(start_random, stop_random + 1)
 
-print('Как думаете какое число получилось?')
+print('Угадайте какое число получилось?')
 
 guess = int(input('Ведите его: '))
 
 if random_number == guess:
     print(f'Вы угадали, это было число {random_number} !\nПоздравляем!')
 else:
-    print(f'К сожалению вы не угадали, слуйчайным числом было {random_number}!')
+    print(f'К сожалению вы не угадали, это было число {random_number}!')
 
 
