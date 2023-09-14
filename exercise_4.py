fd05128bacedaa0f6b9ab9d66d0471c46b8310c8
forsterit <olegkrivolap57@gmail.com>
2023-09-14 13:10:06 +0300

    Homework lesson 3

--- exercise_4.py
+++ exercise_4.py
@@ -1,24 +1,24 @@
-# TODO через цикл while
-# number = int(input('Введите целое число :'))
-# count = 0
-# cube = 0
-# summ_1 = 0
-#
-# while count < number:
-#     count += 1
-#     cube = count ** 3
-#     summ_1 += cube
-# else:
-#     print(summ_1)
 
-# TODO через цикл for
+number = int(input('Введите целое число :'))
+count = 0
+cube = 0
+summ_1 = 0
 
-# number = int(input('Введите целое число :'))
-# cube = 0
-# summ_1 = 0
-#
-# for i in range(1, number + 1):
-#     cube = i ** 3
-#     summ_1 += cube
-# else:
-#     print(summ_1)
+while count < number:
+    count += 1
+    cube = count ** 3
+    summ_1 += cube
+else:
+    print(summ_1)
+
+
+
+number = int(input('Введите целое число :'))
+cube = 0
+summ_1 = 0
+
+for i in range(1, number + 1):
+    cube = i ** 3
+    summ_1 += cube
+else:
+    print(summ_1)
